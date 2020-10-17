1bb685ce3018592ce766f3517b9a437d990b41c4
Tony Espy <espy@canonical.com>
2020-10-17 09:13:41 -0400

    fix: use kong's bintray deb
    This change updates the snap to use the xenial-based
    debian package published by Kong Inc on bintray.com.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- bin/kong-daemon.sh
+++ bin/kong-daemon.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
+KONG=/usr/local/bin/kong
 # run kong migrations up to bootstrap the cassandra database
 # note that sometimes cassandra can be in a "starting up" state, etc.
 # and in this case we should just loop and keep trying
 # we don't implement a timeout here because systemd will kill us if we 
 # don't succeed in 15 minutes (or whatever the configured stop-timeout is)
-until kong migrations bootstrap --conf "$KONG_CONF"; do
+until $KONG migrations bootstrap --conf "$KONG_CONF"; do
     sleep 5
 done
 
 # now start kong normally
-kong start --conf "$KONG_CONF" --v
+$KONG start --conf "$KONG_CONF" --v
